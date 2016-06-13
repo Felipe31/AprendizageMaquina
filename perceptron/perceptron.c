eb21c8de81227090bfc75efdcc3b8a8fa23d537c
Felipe Soares <felipe31soares@gmail.com>
2016-06-13 10:03:03 -0300

    Inclusão perceptron

--- perceptron/perceptron.c
+++ perceptron/perceptron.c
@@ -4,12 +4,11 @@
 
 typedef int int;
 
-
 void instancia(int *x, int i, float *w, int *bias)
 {
     srand(time(NULL));
     float e[i-1];
-    int aprend, cont, u = 0, y, erro = 1, bias = 1;
+    int aprend, cont, u = 0, y, erro = 1;
 
     while (!erro)
     {
@@ -31,19 +30,18 @@ void instancia(int *x, int i, float *w, int *bias)
 }
 
 
-
-
-
-int perceptron (int * x, int i)
+int perceptron (int ** x, int i, int j)
 {
-    float *w = (float *)malloc(sizeof(float)*)
-    int bias=0;
+    float *w = (float *)malloc(sizeof(float)*(i-1));
+    int *bias = (int *)malloc(sizeof(int));
     cont = 0;
     while(cont < i-1)
     {
         w[cont] = rand()%1 + 0.5;
     }
 
+    for(cont = 0; cont < i; cont++)
+        instancia(x+cont, j, w, bias);
 
     return 0;
 }
